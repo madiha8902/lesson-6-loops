7f49d03dedbee5a15d68e70e7516e69f4e91eb1c
madiha8902 <madiha.batool@bellfield.nsw.edu.au>
2024-10-03 19:08:42 +1000

    loops in pyton

---
+++ main.py
@@ -0,0 +1,14 @@
+# str="Madiha"
+# for i in str:
+#     print(i)
+
+# for i in range(0,11,2):
+#     print(i)
+# number = int(input("enter number for table"))
+# for i in range(0,11):
+#     print(number,"x",i,"=",number * i)
+    
+
+# sum of the natural numbers
+sum= sum(range(1,11))
+print(sum)
